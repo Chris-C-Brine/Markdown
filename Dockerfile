8278cd515cce4a1a5e97626e2cf5b13c979fb7dc
Christopher Brine <32967508+Chris-C-Brine@users.noreply.github.com>
2022-11-26 02:45:15 +0000

    watch files configured & dev server - on startup

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM mcr.microsoft.com/devcontainers/typescript-node:18-bullseye
 WORKDIR /workspaces/react-note-taking-app
-COPY ./package.json .
+COPY ./package*.json ./
 RUN npm install --quiet
 COPY ./ .
-CMD ["npm", "run", "build"]
+RUN npm install
+RUN npm run build
+CMD ["serve", "dist/"]
 EXPOSE 3000
--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
   },
   build: {
-    watch: {
-      include: 'src/**'
-    }
-  }
+    rollupOptions: {
+      watch: {
+        include: 'src/**',
+      },
+    },
+  },
 })
